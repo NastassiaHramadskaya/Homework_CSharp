42c5bd42d4912e527cd927fa2ba3c91488677aef
Nastassia Hramadskaya <anastasiyagromadskaya@gmail.com>
2022-11-30 21:58:51 +0300

    add task 23 in homework3

---
+++ Homework3_CSharp/Task23/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+//методвы вывода кубов чисел от 1 до введенного
+Console.Clear();
+
+void TableOfCubed(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    int i = 1;
+    if (number < 1)
+    {
+        Console.WriteLine("Введенное число не соответствует условиям задачи. Введите число от 1 и более.");
+    }
+    else
+    {
+        Console.WriteLine("Таблица кубов для чисел от 1 до " + number + ": ");
+        while (i <= number)
+        {
+        Console.Write(Math.Pow(i, 3) + ", ");
+        i++;
+        }
+    }
+    Console.WriteLine();
+}
+
+TableOfCubed("Введите число: ");
