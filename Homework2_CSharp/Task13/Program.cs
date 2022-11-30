ddd0a402b2810182505f8e3fe93fa699855f7aac
Nastassia Hramadskaya <anastasiyagromadskaya@gmail.com>
2022-11-30 21:08:09 +0300

    add homework2

---
+++ Homework2_CSharp/Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()); //перевод введенной строки в число
+int numberShort = number; //добавление переменной для отсечения разрядов
+int thirdDigit = 0;
+if (number < 0)
+{
+    numberShort = number * (-1); //корректировка отрицательного числа на положительного для упрощения работы
+}
+if (numberShort < 100)
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+else
+{while (numberShort > 1000) //цикл для сокращения числа до трехзначного
+    {
+        numberShort = numberShort / 10;
+    }
+    thirdDigit = numberShort % 10;
+    Console.WriteLine($"{number} -> {thirdDigit}");
+}
---
+++ Homework2_CSharp/Task13_Another_Solution/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()); //перевод введенной строки в число
+if (number < 0)
+{
+    number = number * (-1); //корректировка отрицательного числа на положительного для упрощения работы
+}
+string numberA = number.ToString();
+int N = numberA.Length;
+int[] array = {};
+int index = 0;
+while (index < N)
+{
+    array[index] = number %10;
+    index = index - 1;
+}
+Console.WriteLine(array[N - 4]);
+
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         index++;
+//     }
+// }
