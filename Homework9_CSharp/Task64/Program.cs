d406360cb4802a422d42d43501cee08f828595dc
Nastassia Hramadskaya <anastasiyagromadskaya@gmail.com>
2022-12-25 15:49:52 +0300

    add task 64 in homework9

---
+++ Homework9_CSharp/Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// метод ввода числа пользователем
+int DataEntry(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//метод вывода натуральных чисел от N до 1
+void ShowNumbers(int number)
+{
+    Console.Write($"{number} ");
+    if (number > 1)
+    {
+        ShowNumbers(number - 1);
+    }
+}
+
+int number = DataEntry("Введите число N: ");
+Console.WriteLine();
+ShowNumbers(number);
